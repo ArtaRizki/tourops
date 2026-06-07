e5cb118c7d133712187bb51ace9a558e977b5093
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-06-08 06:44:39 +0700

    Update deploy script

--- scratch/deploy.ts
+++ scratch/deploy.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { Client } from "ssh2";
 
 const conn = new Client();
@@ -24,10 +25,9 @@ conn.on("ready", () => {
     });
 
     // Write commands one after another
-    stream.write("cd ~/tourops-src\n");
+    stream.write("cd tourops\n");
     stream.write("git pull origin main\n");
     stream.write("docker build -t tourops:latest .\n");
-    stream.write("cd ~/tourops\n");
     stream.write("docker compose down\n");
     stream.write("docker compose up -d\n");
     stream.write("exit\n");
